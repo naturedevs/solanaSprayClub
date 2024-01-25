09d7d032e824a8e4382012d6accceeb85f8396bf
HarryJulin <naturesky0411@gmail.com>
2024-01-25 18:07:36 +0800

    complete

--- src/pages/sections/Header.js
+++ src/pages/sections/Header.js
@@ -1,17 +1,25 @@
 import {Link} from 'react-router-dom'
 import {Typography} from "@material-tailwind/react"
 import icons from '../../assets/svg/5icons.svg'
+import icon_twitter from '../../assets/img/ico_twitter.png'
+import ico_discord from '../../assets/img/ico_discord.png'
+import ico_instgram from '../../assets/img/ico_instgram.png'
+import ico_tiktok from '../../assets/img/ico_tiktok.png'
+import ico_youtube from '../../assets/img/ico_youtube.png'
 import {Disclosure} from '@headlessui/react'
-import { useState } from 'react'
+import {useState} from 'react'
 
 export default function Header() {
-    const [open, setOpen] = useState(false);
+    const [open,
+        setOpen] = useState(false);
 
     const handleClick = () => {
         setOpen(!open);
     }
     return (
-        <div as="div" className="flex flex-row justify-between overflow-hidden w-full bg-black text-white h-[100px] items-center px-10">
+        <div
+            as="div"
+            className="flex flex-row justify-between overflow-hidden w-full bg-black text-white h-[100px] items-center px-10">
             <div className="hidden xl:block">
                 <Link to="/" className='px-3 text-3xl'>Home</Link>
                 <Link to="/" className='px-3 text-3xl'>Mint</Link>
@@ -22,7 +30,11 @@ export default function Header() {
             </div>
             <div className="flex xl:hidden justify-end flex-col" onClick={handleClick}>
                 <span className="text-xl  navbar-toggler-icon"></span>
-                <div className={`absolute w-screen h-screen left-0 top-0 z-50 bg-white flex flex-col text-black transition-all ${open?"left-0":"-left-full"}`}>
+                <div
+                    className={`absolute  sm:w-[70%] md:w-[50%] w-screen h-screen left-0 top-0 z-50 bg-black flex flex-col text-white transition-all ${open
+                    ? "left-0"
+                    : "-left-[110%]"}`}>
+                    <br/>
                     <br/>
                     <br/>
                     <Link to="/" className='px-3 text-3xl'>Home</Link>
@@ -34,11 +46,26 @@ export default function Header() {
                 </div>
             </div>
             <div className='flex flex-col gap-2 pt-3 items-center pr-5'>
-                <div className='border-2 border-gray-500 rounded-md text-3xl text-center px-3 py-1 cursor-pointer'>
+                <div
+                    className='border-2 border-gray-500 rounded-md text-3xl text-center px-3 py-1 cursor-pointer'>
                     Connect Wallet
                 </div>
                 <div className='flex flex-row'>
-                    <img src={icons} className='w-[180px]'></img>
+                    <Link to="/">
+                        <img src={icon_twitter} className='w-[42px] px-1 '></img>
+                    </Link>
+                    <Link to="/">
+                        <img src={ico_instgram} className='w-[42px] px-1'></img>
+                    </Link>
+                    <Link to="/">
+                        <img src={ico_youtube} className='w-[42px] px-1'></img>
+                    </Link>
+                    <Link to="/">
+                        <img src={ico_tiktok} className='w-[42px] px-1'></img>
+                    </Link>
+                    <Link to="/">
+                        <img src={ico_discord} className='w-[42px] px-1'></img>
+                    </Link>
                 </div>
             </div>
         </div>
